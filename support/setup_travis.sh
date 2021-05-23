3aece3b51b16e1e2e4449ed6acae5205d0d2c9da
Ben Webb <ben@salilab.org>
2021-05-22 17:02:18 -0700

    Use new imp-nightly package that uses conda-forge

--- support/setup_travis.sh
+++ support/setup_travis.sh
@@ -2,40 +2,30 @@
 
 # Set up an environment to run tests under Travis CI (see ../.travis.yml)
 
-if [ $# -ne 3 ]; then
-  echo "Usage: $0 conda_dir imp_branch python_version"
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 imp_branch python_version"
   exit 1
 fi
 
 cur_dir=$(pwd)
-conda_dir=$1
-imp_branch=$2
-python_version=$3
+imp_branch=$1
+python_version=$2
 temp_dir=$(mktemp -d)
 
 if [ ${imp_branch} = "develop" ]; then
   IMP_CONDA="imp-nightly"
+  # get conda-forge, not main, packages
+  conda config --remove channels defaults
+  conda config --add channels conda-forge
 else
   IMP_CONDA="imp"
 fi
 
 cd ${temp_dir}
 
-# Use miniconda Python rather than the Travis environment
-
-# Clean up after a potential previous install failure
-rm -rf ${conda_dir}
-# Save on some downloading if the version is the same
-if [ "${python_version}" == "2.7" ]; then
-  wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
-else
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-fi
-bash miniconda.sh -b -p ${conda_dir}
-export PATH=${conda_dir}/bin:$PATH
-conda update --yes -q conda
 conda create --yes -q -n python${python_version} -c salilab python=${python_version} pip nose ${IMP_CONDA} gxx_linux-64 eigen swig cmake
-source activate python${python_version}
+eval "$(conda shell.bash hook)"
+conda activate python${python_version}
 pip install coverage
 
 # IMP tests use sys.argv[0] to determine their location, which won't work if
