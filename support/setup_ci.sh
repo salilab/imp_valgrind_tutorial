96ea4bb9a6e96b9c8e0fb25871b7c59b85fb85ee
Ben Webb <ben@salilab.org>
2024-12-12 11:12:04 -0800

    Update to IMP 2.22.0, drop Python 2

--- support/setup_ci.sh
+++ support/setup_ci.sh
@@ -18,24 +18,8 @@ if [ ${imp_branch} = "develop" ]; then
 else
   IMP_CONDA="imp"
 fi
-if [ ${python_version} = "2.7" ]; then
-  BOOST=""
-  pip="pip<=19.3.1"
-  # Python.h includes crypt.h, which is no longer provided by default
-  crypt="libxcrypt"
-else
-  BOOST="libboost-devel"
-  pip="pip"
-  crypt=""
-fi
-conda create --yes -q -n python${python_version} -c salilab python=${python_version} ${pip} ${crypt} ${IMP_CONDA} ${BOOST} gxx_linux-64 eigen cereal swig cmake numpy
+conda create --yes -q -n python${python_version} -c salilab python=${python_version} pip ${IMP_CONDA} libboost-devel gxx_linux-64 eigen cereal swig cmake numpy
 eval "$(conda shell.bash hook)"
 conda activate python${python_version}
 
-if [ ${python_version} = "2.7" ]; then
-  # pytest-flake8 1.1.0 tries to import contextlib.redirect_stdout, which
-  # isn't present in Python 2
-  pip install pytest-cov coverage 'pytest-flake8<1.1'
-else
-  pip install pytest-cov coverage pytest-flake8
-fi
+pip install pytest-cov coverage pytest-flake8
